63b587082733fd323da4c9134e965dcdd344f96a
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-05-14 11:35:04 +0200

    Add toggle for cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -119,6 +119,8 @@ const templates = {
 
     getElements(){ //(REFERENCJE)
       const thisProduct = this;
+
+      thisProduct.dom = {};
     
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
@@ -299,16 +301,19 @@ const templates = {
 
       thisWidget.input.addEventListener('change', function(event){
         event.preventDefault();
+
         thisWidget.setValue(thisWidget.input.value);
       });
 
       thisWidget.linkDecrease.addEventListener('click', function(event){
         event.preventDefault();
+
         thisWidget.setValue(thisWidget.value - 1);
       }); 
       
       thisWidget.linkIncrease.addEventListener('click', function(event){
         event.preventDefault();
+
         thisWidget.setValue(thisWidget.value + 1);
       });  
     }
@@ -321,6 +326,39 @@ const templates = {
     }
     
   } 
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
 
   const app = {
     initMenu: function() {
@@ -337,6 +375,13 @@ const templates = {
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -347,7 +392,11 @@ const templates = {
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+    
+    
   };
 
   app.init();

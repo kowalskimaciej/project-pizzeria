da2503aff1f4cae6c4a3bc82bd264e18cbc140de
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-06-22 14:14:32 +0200

    Add Add home and class

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
+import Home from './components/Home.js';
+
 
 const app = {
   initPages: function() {
@@ -106,6 +108,13 @@ const app = {
     thisApp.booking = new Booking(bookingContainer);
   },
 
+  initHome: function() {
+    const thisApp = this;
+
+    const homeContainer = document.querySelector(select.containerOf.home);
+    thisApp.home = new Home(homeContainer);
+  },  
+
   init: function(){
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -118,6 +127,7 @@ const app = {
     thisApp.initData();
     thisApp.initCart();
     thisApp.initBooking();
+    thisApp.initHome();
   },
 };
 
